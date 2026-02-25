29f1532c43a16e17d13aa206fca700045754123b
lisalibs <lisadllibby@gmail.com>
2026-02-25 18:31:32 -0500

    Add electronics assets and update dependencies
    - Added iPad and shopping images to Electronics assets
    - Updated app configuration and dependencies
    - Updated SignUpScreen and FirebaseConfig

--- src/services/FirebaseConfig.js
+++ src/services/FirebaseConfig.js
@@ -8,6 +8,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
   projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
