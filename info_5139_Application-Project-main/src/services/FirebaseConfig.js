ec111b658166fec4ab4bd3f80c2880b9932c150c
Mary <mec22denzie@yahoo.com>
2026-07-21 15:12:49 -0400

    Configure Firebase Hosting and update Expo web deployment

--- info_5139_Application-Project-main/src/services/FirebaseConfig.js
+++ info_5139_Application-Project-main/src/services/FirebaseConfig.js
@@ -7,14 +7,13 @@ import { getStorage } from "firebase/storage";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCjvBYAMcoTFGkMIOPWL5QqFt5GCYQQvDs",
+  authDomain: "info5139-falconshop.firebaseapp.com",
+  projectId: "info5139-falconshop",
+  storageBucket: "info5139-falconshop.firebasestorage.app",
+  messagingSenderId: "797759027695",
+  appId: "1:797759027695:web:ae555755c1fbb57cb1de1a",
+  measurementId: "G-Y1T0SG7NWK"
 };
 
 
